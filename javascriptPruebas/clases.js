8519b4fcdcddf0a6bd60ea00be61207c0fa3aae4
moviedo <moviedo@koencta-group.com>
2024-11-17 16:34:40 -0300

    javascriptPruebas add clases to index19

--- javascriptPruebas/clases.js
+++ javascriptPruebas/clases.js
@@ -1,39 +1,47 @@
 
- class Socios {
-    constructor(nroSocio, name,adherentes) {
-      this.nroSocio = nroSocio;
-      this.name = name;
-      this.adherentes = adherentes;
-    }
+class Socios {
+  constructor(nroSocio, name, adherentes) {
+    this.nroSocio = nroSocio;
+    this.name = name;
+    this.adherentes = adherentes;
+  }
 
+
+}
+
+class Adherentes {
+  constructor(orden, name) {
+    this.orden = orden;
+    this.name = name;
 
   }
 
-   class Adherentes {
-    constructor(orden, name) {
-      this.orden = orden;
-      this.name = name;
-      
-    }
 
+}
 
+class Animal {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
   }
 
-  class Animal {
-    constructor(nombre) {
-      this.nombre = nombre;
-    }
-  
-    hablar() {
-      console.log(this.nombre + ' hace un ruido.');
-    }
+  hacerSonido() {
+    console.log(`${this.nombre} está haciendo un sonido.`);
   }
-  
-  class Perro extends Animal {
-    hablar() {
-      console.log(this.nombre + ' ladra.');
-    }
+}
+
+class Perro extends Animal {
+  constructor(nombre, edad, raza) {
+    super(nombre, edad); // Llama al constructor de la clase base
+    this.raza = raza;
   }
+  hacerSonido() {
+    console.log(`${this.nombre} dice: ¡Guau!`);
+  }
+  moverCola() {
+    console.log(`${this.nombre} está moviendo la cola.`);
+  }
+}
 
 // 👇️ named export (same as previous code snippet)
-export {Socios,Adherentes};
+export { Socios, Adherentes, Perro };
---
+++ javascriptPruebas/index19.js
@@ -0,0 +1,7 @@
+import {Perro } from './clases'
+const perro = new Perro("Firulais", 3, "Labrador");
+
+
+// Usar las instancias
+perro.hacerSonido(); // Firulais dice: ¡Guau!
+perro.moverCola(); // Firulais está moviendo la cola.
